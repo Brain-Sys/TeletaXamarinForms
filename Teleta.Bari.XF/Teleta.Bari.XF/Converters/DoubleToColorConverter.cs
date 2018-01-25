60bfd852ef57a70b3797255632addc87a5a4e479
Damiani Liborio Igor <igor.damiani@hotmail.com>
2018-01-25 16:10:30 +0100

    -

---
+++ Teleta.Bari.XF/Teleta.Bari.XF/Converters/DoubleToColorConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Teleta.Bari.XF.Converters
+{
+    public class DoubleToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double v = (double)value;
+
+            if (v < 20)
+            {
+                return Color.Green;
+            }
+            else if (v >= 20 && v <= 80)
+            {
+                return Color.Orange;
+            }
+
+            return Color.BlueViolet;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
