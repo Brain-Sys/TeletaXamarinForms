04e3a9d12344990087912a85855dee537df65108
Damiani Liborio Igor <igor.damiani@hotmail.com>
2018-01-26 10:03:04 +0100

    -

--- Teleta.Bari.ViewModels/ArticleDetailViewModel.cs
+++ Teleta.Bari.ViewModels/ArticleDetailViewModel.cs
@@ -3,6 +3,7 @@ using GalaSoft.MvvmLight.Command;
 using Plugin.TextToSpeech.Abstractions;
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 using Teleta.Bari.XF.Repository;
 
@@ -10,6 +11,8 @@ namespace Teleta.Bari.ViewModels
 {
     public class ArticleDetailViewModel : ViewModelBase
     {
+        private HttpClient network;
+
         private Article article;
         public Article Article
         {
@@ -22,10 +25,19 @@ namespace Teleta.Bari.ViewModels
         }
 
         public RelayCommand SpeakCommand { get; set; }
+        public RelayCommand DownloadCommand { get; set; }
 
         public ArticleDetailViewModel()
         {
             this.SpeakCommand = new RelayCommand(SpeakCommandExecute);
+            this.DownloadCommand = new RelayCommand(DownloadCommandExecute);
+            network = new HttpClient();
+        }
+
+        private void DownloadCommandExecute()
+        {
+            string uri = "http://tsdemo.dnsalias.org:5515/ws_logistica.asmx?op=WS_TSP_Arca_Get_AR_JSON";
+            network.PostAsync(uri);
         }
 
         private async void SpeakCommandExecute()
